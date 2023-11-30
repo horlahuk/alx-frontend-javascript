1e290f3b98a3e0a357c7566d5f5c244ea266611d
horlahuk <olaitan.u.k@gmail.com>
2023-11-30 22:43:06 +0100

    add task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from "./5-building";
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
